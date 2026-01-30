ea32120e8a6ab5930823a65781d7c3996267ec96
vickysrana2-cloud <vickysrana2@gmail.com>
2026-01-30 22:42:13 +0530

    base url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://petcare-app-backend-production.up.railway.app/api",
+  baseURL: "https://pet-care-app-backend-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
