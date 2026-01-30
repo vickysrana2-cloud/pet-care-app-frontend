b201a603852f7fcbdce99c68b05fa4f1b00a706a
vickysrana2-cloud <vickysrana2@gmail.com>
2026-01-30 18:19:52 +0530

    base api changed

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pet-care-app-backend-production.up.railway.app/api",
+  baseURL: "https://petcare-app-backend-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
